a7c62a1cb20bbfee7dcf3d65f67d54131761dc07
handbyhand <mr.handbyhand666@gmail.com>
2020-08-12 23:27:26 +0800

    init

---
+++ app.py
@@ -0,0 +1,72 @@
+# encoding: utf-8
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,VideoSendMessage,ImageSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton
+)
+
+app = Flask(__name__)
+
+# you can replace by load env file
+handler = WebhookHandler('Your_Channel_Secret') 
+line_bot_api = LineBotApi('Your_Channel_Access_Token') 
+
+
+@app.route('/')
+def index():
+    return "<p>Hello World!</p>"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+# ========== handle user message ==========
+@handler.add(MessageEvent, message=TextMessage)  
+def handle_text_message(event):
+    # message from user                  
+    msg = event.message.text
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg))
+
+
+    
+
+
+import os
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
+    
